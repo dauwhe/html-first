7fa8f3b9bde00a6bc317d56250b8f803f45074c8
Benjamin Young <byoung2@wiley.com>
2017-08-15 12:02:46 -0400

    Show keep button or offlined message

--- book.js
+++ book.js
@@ -15,8 +15,10 @@ function registerServiceWorker() {
         console.log('Service worker installed');
       } else if(reg.active) {
         console.log('Service worker active');
+        fauxFramePrimaryResources();
       }
-
+      // setup display options on the toolbar
+      checkRegistration();
     }).catch(function(error) {
       // registration failed
       console.error('Registration failed with ' + error);
@@ -42,12 +44,40 @@ function fauxFramePrimaryResources() {
   });
 }
 
-var button = document.createElement('button');
+let button = document.createElement('button');
+button.id = 'keep-web-publication';
 button.innerText = 'Keep This Book (offline)';
-
 // populate the cache with the bits from the nav
 button.onclick = function() {
   registerServiceWorker();
 }
-// TODO: add a .prepend() shim...this doesn't work in Edge...yet?
-document.body.prepend(button);
+
+let message = document.createElement('div');
+message.id = 'kept-web-publication';
+message.style.background = '#efefef';
+message.innerText = 'You have kept a copy of this Web Publication.';
+
+// TODO: Add update button
+
+let toolbar = document.createElement('nav');
+toolbar.id = 'web-publication-toolbar';
+toolbar.style.border = '1px solid blue';
+toolbar.style.display = 'none';
+toolbar.append(button, message);
+
+// TODO: add .append()/.prepend() shims...this doesn't work in Edge...yet?
+document.body.prepend(toolbar);
+
+function checkRegistration() {
+  navigator.serviceWorker.getRegistration().then((reg) => {
+    if (undefined !== reg) {
+      message.style.display = '';
+      button.style.display = 'none';
+    } else {
+      message.style.display = 'none';
+      button.style.display = '';
+    }
+    toolbar.style.display = '';
+  });
+}
+checkRegistration();
